81006c700c68e0808d20850eeddcd208d0cbfc6e
goodzzong <goodzzong@gmail.com>
2018-05-02 11:23:45 +0900

    add tax attach_files

---
+++ database/migrations/2018_05_02_110217_add_tax_and_attach_files_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTaxAndAttachFilesToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sales', function (Blueprint $table) {
+            $table->enum('tax',['1','2'])->defalut(1)->after('collectPrice10');
+            $table->text('attach_files')->comment('첨부파일')->after('tax');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('sales', function (Blueprint $table) {
+            $table->dropColumn(['tax', 'attach_files']);
+        });
+    }
+}
